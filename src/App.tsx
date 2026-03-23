af0687e72e3c4cc252689f8ff592a52e4a31e5be
Se3cat <nvan57605@gmail.com>
2026-03-23 14:35:31 +0700

    update

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,46 @@
 import { Toaster } from "react-hot-toast";
-import { Link } from "react-router-dom";
-import { Form, Input, Button } from "antd";
+import { Link, Route, Routes } from "react-router-dom";
+import { Form, Input, Button, Table, InputNumber, Select } from "antd";
 import { Layout } from "antd";
-// import Dashboard from "./pages/lab1";
-// import StudentList from "./pages/lab2";
-// import Lab2 from "./pages/lab2";
-// import LoginForm from "./pages/Lab3";
-import Lab4 from "./pages/lab4";
-import StoryList from "./pages/lab5";
+import { Image } from "antd";
+// import StoryForm from "./pages/Lab4";
+// import StoryList from "./pages/Lab5";
+import { EditStory } from "./pages/lab6";
 const { Header, Content, Footer } = Layout;
 
 function App() {
   const onFinish = (values: any) => {
     console.log(values);
   };
+
+  const columns = [
+    { title: "Họ tên", dataIndex: "name" },
+    { title: "Tuổi", dataIndex: "age" },
+    { title: "Môn học", dataIndex: "subject" },
+    {
+      title: "Avatart",
+      dataIndex: "avatar",
+      render: (avatar: string) => <Image width={50} src={avatar} />,
+    },
+  ];
+
+  const data = [
+    {
+      key: 1,
+      name: "John",
+      age: 25,
+      subject: "FE2",
+      avatar: "https://i.pravatar.cc/150?img=1",
+    },
+    {
+      key: 2,
+      name: "Anna",
+      age: 30,
+      subject: "FE1",
+      avatar: "https://i.pravatar.cc/150?img=2",
+    },
+  ];
+
   return (
     <>
       <nav className="bg-blue-600 text-white shadow">
@@ -48,46 +75,76 @@ function App() {
       {/* MAIN CONTENT */}
       <div className="max-w-6xl mx-auto mt-10 px-4 text-center">
         <h1 className="text-4xl font-bold mb-4">Chào mừng đến với WEB2091</h1>
-        
-        
-        <Button type="primary">Click me</Button>
-        <Button type="default">Click me</Button>
-        <Button type="dashed">Click me</Button>
-        <Button type="link">Click me</Button>
-        <Button type="text">Click me</Button>
+        <Routes>
+          <Route path="/edit/:id" element={<EditStory />}></Route>
+        </Routes>
         <Layout>
-          <Header style={{ color: "white" }}>Header</Header>
+          {/* <Header style={{ color: "white" }}>Header</Header> */}
           <Content style={{ padding: 20 }}>
-            <Form onFinish={onFinish}>
-              <Form.Item label="Email" name="email"  rules={[
-    { required: true, message: "Vui lòng nhập email" },
-    { type: "email", message: "Email không hợp lệ" },
-  ]}>
-                <Input />
+            {/* <StoryList /> */}
+            {/* <StoryForm /> */}
+            {/* <Form
+              layout="vertical"
+              onFinish={onFinish}
+              style={{ maxWidth: 400 }}
+            >
+              <Form.Item
+                label="Price"
+                name="price"
+                rules={[
+                  { min: 0, type: "number", message: "Gia phai lon hon khong" },
+                ]}
+              >
+                <InputNumber />
               </Form.Item>
-              <Form.Item label="Password" name="password">
+
+              <Form.Item
+                label="Password"
+                name="password"
+                rules={[
+                  { required: true, message: "Please input your password!" },
+                  {
+                    min: 6,
+                    message: "Password must be at least 6 characters!",
+                  },
+                ]}
+              >
                 <Input.Password />
               </Form.Item>
-              
               <Form.Item>
-                <Button htmlType="submit" type="primary">
-                  Submit
+                <Select
+                  options={[
+                    {
+                      value: "FE1",
+                      label: "FE1",
+                    },
+                    {
+                      value: "FE2",
+                      label: "FE2",
+                    },
+                  ]}
+                  placeholder="Chọn môn học"
+                />
+              </Form.Item>
+              <Form.Item>
+                <Button type="primary" htmlType="submit">
+                  Đăng nhập
                 </Button>
               </Form.Item>
-            </Form>
-            {/* <LoginForm/> */}
-            {/* <Lab2/> */}
-            <Lab4/>
-            <StoryList/>
+            </Form> */}
+            {/* <Table
+              columns={columns}
+              dataSource={data}
+              pagination={{ pageSize: 1 }}
+            /> */}
           </Content>
-          <Footer>Footer</Footer>
+          {/* <Footer>Footer</Footer> */}
         </Layout>
       </div>
 
       <Toaster />
     </>
   );
-
 }
 
 export default App;
